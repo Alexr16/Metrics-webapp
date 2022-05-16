d63600c2c3193e12b6538fdf67ea8838c40cfd8e
Alexr16 <j.alex-1996@hotmail.com>
2022-05-16 12:27:32 -0500

    Add HomePage route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/Countries/:countryName" element={<CountriesPage />} /> */}
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
