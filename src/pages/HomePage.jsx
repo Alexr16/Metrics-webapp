7f66cc7c689b90a3c2b9b7aca50bc6f5d35672c7
Alexr16 <j.alex-1996@hotmail.com>
2022-05-16 18:21:50 -0500

    Create homePage components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,62 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { GrMicrophone } from 'react-icons/gr';
+import { FiSettings } from 'react-icons/fi';
+import { AiOutlineLeft } from 'react-icons/ai';
 import { fetchCountriesData } from '../Redux/Countries/Countries';
+import './HomePage.scss';
+import { getMapUrl } from '../Utilities/Map';
+import CountriesList from '../components/CountriesList';
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const Countries = useSelector((state) => state.Countries);
+  const { status, data } = useSelector((state) => state.Countries);
+  console.log(data);
   useEffect(() => {
-    if (Countries.length <= 0) {
-      dispatch(fetchCountriesData('Spain'));
-    }
+    // if (data.length <= 0) {
+    dispatch(fetchCountriesData());
+    // }
   }, []);
+
+  const worldMap = getMapUrl('world');
+
+  return (
+    <>
+      <div className="header-container">
+        <div className="ouline-left">
+          <AiOutlineLeft />
+          <h3>2022</h3>
+        </div>
+        <h4 className="center-title">Most Views</h4>
+        <div className="header-buttons">
+          <button type="button" className="microphone" label="microphone"><GrMicrophone /></button>
+          <button type="button" className="settings" label="Settings"><FiSettings /></button>
+        </div>
+      </div>
+      { status === 'success' && (
+      <>
+        <div className="firts-card">
+          <div className="map-container">
+            <img src={worldMap} alt="World Map" className="map" />
+          </div>
+          <div>
+            <h1>Daily Statistics</h1>
+            <h2>{data[32].name}</h2>
+            <p>
+              {`${data[32].confirm} new infections`}
+            </p>
+          </div>
+        </div>
+        <div className="stast">
+          STATS BY COUNTRY
+        </div>
+        <div className="countries-container">
+          <CountriesList countries={data} />
+        </div>
+      </>
+      )}
+    </>
+  );
 };
 
 export default HomePage;
