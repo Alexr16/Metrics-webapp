89d06898bd96acb9d6aac7b32decaff7e8a7975f
Alexr16 <j.alex-1996@hotmail.com>
2022-05-16 18:20:40 -0500

    Add countries into countriesData array

--- src/Redux/Countries/Countries.js
+++ src/Redux/Countries/Countries.js
@@ -3,11 +3,26 @@ const initialState = [];
 
 const date = (new Date()).toISOString().split('T')[0];
 
-export const fetchCountriesData = (country) => async (dispatch) => {
-  fetch(`https://api.covid19tracking.narrativa.com/api/${date}/country/${country}`)
+export const success = (countries) => ({
+  type: FETCH_COUNTRIES_DATA,
+  countries,
+});
+
+export const fetchCountriesData = () => async (dispatch) => {
+  fetch(`https://api.covid19tracking.narrativa.com/api/${date}`)
     .then((data) => data.json())
     .then((data) => {
-      dispatch({ type: FETCH_COUNTRIES_DATA, data });
+      const countriesData = [];
+      const countriesObject = data.dates[date].countries;
+      Object.keys(countriesObject).forEach((country) => {
+        countriesData.push({
+          id: countriesObject[country].id,
+          name: countriesObject[country].name,
+          confirm: countriesObject[country].today_new_confirmed,
+        });
+      });
+
+      dispatch(success(countriesData));
     })
     .catch((error) => { throw error; });
 };
@@ -24,7 +39,7 @@ export const fetchCountriesData = (country) => async (dispatch) => {
 const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_COUNTRIES_DATA:
-      return action.data;
+      return { data: action.countries, status: 'success' };
 
       // case RESERVATION:
       //   return [...state.map((mission) => {
