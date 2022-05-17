b5cb553ea4f720e5a8676605f1a5e8c33cc9e0bf
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 16:53:34 -0500

    Add input to filter data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
-import React, { useEffect } from 'react';
-import { GrMicrophone } from 'react-icons/gr';
+import React, { useEffect, useState } from 'react';
+import { IoMdMic } from 'react-icons/io';
 import { FiSettings } from 'react-icons/fi';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { fetchCountriesData } from '../Redux/Countries/Countries';
@@ -11,12 +11,21 @@ import CountriesList from '../components/CountriesList';
 const HomePage = () => {
   const dispatch = useDispatch();
   const { status, data } = useSelector((state) => state.Countries);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    // if (status === 'Not Fetched') {
     dispatch(fetchCountriesData());
-    // }
   }, []);
+
+  const filterData = (e) => {
+    const string = e.target.value.toLowerCase();
+    const info = data.filter(((country) => (country.id === string)));
+    if (info !== 0) {
+      setSearch(info);
+    } else {
+      setSearch(data);
+    }
+  };
 
   const worldMap = getMapUrl('world');
 
@@ -29,7 +38,7 @@ const HomePage = () => {
         </div>
         <h4 className="center-title">Most Views</h4>
         <div className="header-buttons">
-          <button type="button" className="microphone" label="microphone"><GrMicrophone /></button>
+          <button type="button" className="microphone" label="microphone"><IoMdMic /></button>
           <button type="button" className="settings" label="Settings"><FiSettings /></button>
         </div>
       </div>
@@ -40,18 +49,20 @@ const HomePage = () => {
             <img src={worldMap} alt="World Map" className="map" />
           </div>
           <div>
-            <h1>Daily Statistics</h1>
+            <h1>DAILY STATISTICS</h1>
             <h2>{data[32].name.toUpperCase()}</h2>
             <p>
               {`${data[32].confirm} new infections`}
             </p>
+            <input className="input" type="text" name="search" onChange={filterData} placeholder="Country Name" />
           </div>
         </div>
         <div className="stast">
           STATS BY COUNTRY
         </div>
         <div className="countries-container">
-          <CountriesList countries={data} />
+          { search === ''
+            ? (<CountriesList countries={data} />) : (<CountriesList countries={search} />)}
         </div>
       </>
       )}
