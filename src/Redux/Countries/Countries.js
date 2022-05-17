191dfbe47b46839eeb860a50cad6d85195f906f3
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 14:26:07 -0500

    Destructure data from Api

--- src/Redux/Countries/Countries.js
+++ src/Redux/Countries/Countries.js
@@ -1,7 +1,8 @@
 const FETCH_COUNTRIES_DATA = 'MetricsWebApp/Countries/FETCH_COUNTRIES_DATA';
-const initialState = [];
+const initialState = { status: 'Not Fetched', data: [] };
 
-const date = (new Date()).toISOString().split('T')[0];
+const date = '2022-05-16';
+// (new Date()).toISOString().split('T')[0];
 
 export const success = (countries) => ({
   type: FETCH_COUNTRIES_DATA,
@@ -12,42 +13,46 @@ export const fetchCountriesData = () => async (dispatch) => {
   fetch(`https://api.covid19tracking.narrativa.com/api/${date}`)
     .then((data) => data.json())
     .then((data) => {
-      const countriesData = [];
+      let countriesData = [];
       const countriesObject = data.dates[date].countries;
       Object.keys(countriesObject).forEach((country) => {
         countriesData.push({
           id: countriesObject[country].id,
           name: countriesObject[country].name,
           confirm: countriesObject[country].today_new_confirmed,
+          data: [
+            {
+              source: countriesObject[country].source,
+              today_confirmed: countriesObject[country].today_confirmed,
+              today_deaths: countriesObject[country].today_deaths,
+              today_new_deaths: countriesObject[country].today_new_deaths,
+              today_new_open_cases: countriesObject[country].today_new_open_cases,
+              today_new_recovered: countriesObject[country].today_new_recovered,
+              today_open_cases: countriesObject[country].today_open_cases,
+              today_recovered: countriesObject[country].today_recovered,
+              today_vs_yesterday_confirmed: countriesObject[country].today_vs_yesterday_confirmed,
+              today_vs_yesterday_deaths: countriesObject[country].today_vs_yesterday_deaths,
+              today_vs_yesterday_open_cases: countriesObject[country].today_vs_yesterday_open_cases,
+              today_vs_yesterday_recovered: countriesObject[country].today_vs_yesterday_recovered,
+              yesterday_confirmed: countriesObject[country].yesterday_confirmed,
+              yesterday_deaths: countriesObject[country].yesterday_deaths,
+              yesterday_open_cases: countriesObject[country].yesterday_open_cases,
+              yesterday_recovered: countriesObject[country].yesterday_recovered,
+            },
+          ],
         });
       });
 
+      countriesData = countriesData.filter((country) => country.confirm > 0);
       dispatch(success(countriesData));
     })
     .catch((error) => { throw error; });
 };
 
-// export const fetchCountriesData = () => async (dispatch) => {
-//   fetch('https://api.covid19tracking.narrativa.com/api/countries')
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch({ type: FETCH_COUNTRIES_DATA, data });
-//     })
-//     .catch((error) => { throw error; });
-// };
-
 const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_COUNTRIES_DATA:
       return { data: action.countries, status: 'success' };
-
-      // case RESERVATION:
-      //   return [...state.map((mission) => {
-      //     if (mission.mission_id === action.mission.id) {
-      //       return { ...mission, reserved: action.mission.status };
-      //     }
-      //     return mission;
-      //   })];
 
     default:
       return state;
