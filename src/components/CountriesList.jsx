c75404fd850f6b876167866af5191dbb991292da
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 16:55:07 -0500

    Create ul tag

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,14 +3,14 @@ import Country from './Country';
 const CountriesList = (prop) => {
   const { countries } = prop;
   return (
-    <div className="countries">
+    <ul className="countries">
       {countries.map((country) => (
         <Country
           key={country.id}
           country={country}
         />
       ))}
-    </div>
+    </ul>
   );
 };
 
