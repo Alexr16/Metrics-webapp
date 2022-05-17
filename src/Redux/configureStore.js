b26caf8b0d9f211c9599b0f6433bf6c1756d2a05
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 14:26:16 -0500

    Remove regions

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,11 +2,9 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import countriesReducer from './Countries/Countries';
-import regionReducer from '../pages/CountriesPage';
 
 const rootReducer = combineReducers({
   Countries: countriesReducer,
-  Region: regionReducer,
 });
 
 const store = createStore(
