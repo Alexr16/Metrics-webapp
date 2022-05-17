e77fffd9f7645767c225856a69be85e7b24609aa
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 14:27:38 -0500

    Add link to a dinamyc router

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -6,27 +6,27 @@ import { getMapUrl } from '../Utilities/Map';
 const Country = (prop) => {
   const { country } = prop;
   const image = getMapUrl(`${country.id}`);
+
   return (
     <>
       <Link
-        to={`/Countries/${country.id}`}
-      />
-      <div className="country">
-        <div className="arrow">
-          <button type="button" className="arrow-right" label="Arrow rigth"><BsArrowRightCircle /></button>
+        to={`/Country/${country.id}`}
+      >
+        <div className="country">
+          <div className="arrow">
+            <button type="button" className="arrow-right" label="Arrow rigth"><BsArrowRightCircle /></button>
+          </div>
+          <div className="country-img-container">
+            <img src={image} alt="World Map" className="country-img" />
+          </div>
+          <div className="country-data">
+            <h2 className="country-name">{country.name.toUpperCase()}</h2>
+            <p className="country-p">
+              {country.confirm}
+            </p>
+          </div>
         </div>
-        <div className="country-img-container">
-          <img src={image} alt="World Map" className="country-img" />
-        </div>
-        <div className="country-data">
-          <h2 className="country-name">{country.name}</h2>
-          <p className="country-p">
-            {country.confirm}
-          </p>
-        </div>
-
-      </div>
-
+      </Link>
     </>
   );
 };
