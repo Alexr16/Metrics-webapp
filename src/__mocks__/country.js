ed80206d42f2a2704029c22254516807784701d8
Alexr16 <j.alex-1996@hotmail.com>
2022-05-17 19:34:41 -0500

    Create mock redux-store

---
+++ src/__mocks__/country.js
@@ -0,0 +1,16 @@
+const countryData = {
+  Countries: [
+    {
+      id: 'lkjhg',
+      name: '12s34',
+      confirm: 22,
+    },
+    {
+      id: 'sfsdvfdv',
+      name: '12s3efarf4',
+      confirm: 23,
+    },
+  ],
+};
+
+export default countryData;
